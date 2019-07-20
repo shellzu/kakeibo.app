caf0549d6c8a716f046ce961731da566c5d29f61
Your Name <you@example.com>
2019-07-20 18:33:44 +0900

    addform(addbuttonとform1のmaleculesのつもり)作成中

---
+++ src/components/addform.vue
@@ -0,0 +1,17 @@
+<template>
+<div id="app">
+    <form1></form1>
+    <addbutton v-on:addList="entry"></addbutton>
+</div>
+</template>
+<script>
+import form1 from './form1'
+import addbutton from './addbutton'
+export default {
+  name: 'app',
+  conponents: {
+    form1,
+    addbutton
+  }
+}
+</script>
